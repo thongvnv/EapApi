78a11a5ef9bb3d73c1d7f71f5c765650c18d3f8b
thongvnv <thongnhd00522@fpt.edu.vn>
2018-11-23 10:59:49 +0700

    API

---
+++ EAPApi/Data/EAPApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EAPApi.Models
+{
+    public class EAPApiContext : DbContext
+    {
+        public EAPApiContext (DbContextOptions<EAPApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EAPApi.Models.Employee> Employee { get; set; }
+    }
+}
